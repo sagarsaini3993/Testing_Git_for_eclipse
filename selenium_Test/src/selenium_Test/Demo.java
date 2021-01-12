6abc5dc1e986cff736fff5b4c306bae505d31e4d
sagarsaini3993 <sagarsaini3993@gmail.com>
2021-01-12 00:54:16 -0500

    Initial commit

---
+++ selenium_Test/src/selenium_Test/Demo.java
@@ -0,0 +1,34 @@
+package selenium_Test;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		System.setProperty("webdriver.safari.driver", )
+		
+		// Reverse the array elements
+		
+		int n[] = {1,2,3,4,5,4,4};
+//		
+//		for(int i=n.length-1 ; i>=0; i--) {
+//			System.out.print(n[i]);
+//		}
+		
+		// occurrence of elements of array
+		int k = 4;
+		int num=0;
+		for(int i=0; i<n.length; i++) {
+			
+			if(k==n[i]) {
+				 num++;
+					System.out.print(num);
+
+			}
+			
+			// Delete element at index 2
+		}
+		
+	}
+
+}
